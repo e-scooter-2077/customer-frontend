8c4829dadfc3a9c5027d14b681d0a2f0f8185df0
Francesco Dente <dente.francesco1996@gmail.com>
2021-11-18 19:19:32 +0100

    Initial GUI structure

---
+++ EScooter.CustomerFrontend/CustomerView.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace EScooter.CustomerFrontend
+{
+    public record ScooterViewModel(Guid Id, double Latitude, double Longitude, double BatteryLevel);
+
+    /// <summary>
+    /// Interaction logic for CustomerView.xaml
+    /// </summary>
+    public partial class CustomerView : Window
+    {
+        public CustomerViewModel Customer { get; private set; }
+
+        public CustomerView(CustomerViewModel customer)
+        {
+            InitializeComponent();
+            Customer = customer;
+        }
+
+        private void WindowLoaded(object sender, RoutedEventArgs e)
+        {
+            _titleLabel.Content = $"Customer: {Customer.Id}";            
+        }
+
+        private async Task Refresh()
+        {
+
+        }
+
+        private async Task<IEnumerable<ScooterViewModel>> LoadScooters()
+        {
+            await Task.Yield();
+            return new ScooterViewModel[]
+            {
+                new(Guid.NewGuid(), 10, 30, 10),
+                new(Guid.NewGuid(), 11, 22, 90)
+            };
+        }
+    }
+}
---
+++ EScooter.CustomerFrontend/HomeView.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using static EasyDesk.Tools.Collections.EnumerableUtils;
+
+namespace EScooter.CustomerFrontend
+{
+    public record CustomerViewModel(Guid Id);
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class HomeView : Window
+    {
+        public HomeView()
+        {
+            InitializeComponent();
+        }
+
+        private async void WindowLoaded(object sender, RoutedEventArgs e)
+        {
+            await Refresh();
+        }
+
+        private void LoginClicked(object sender, RoutedEventArgs e)
+        {
+            if (_customersListBox.SelectedItem is not CustomerViewModel selectedCustomer)
+            {
+                MessageBox.Show("No customer is selected");
+                return;
+            }
+            new CustomerView(selectedCustomer).Show();
+        }
+
+        private async void RefreshClicked(object sender, RoutedEventArgs e)
+        {
+            await Refresh();
+        }
+
+        private async Task Refresh()
+        {
+            var customers = await LoadCustomers();
+            _customersListBox.ItemsSource = customers;
+            if (customers.Any())
+            {
+                _customersListBox.SelectedIndex = 0;
+            }
+        }
+
+        private Task<IEnumerable<CustomerViewModel>> LoadCustomers()
+        {
+            return Task.FromResult(Items(
+                new CustomerViewModel(Guid.NewGuid()),
+                new CustomerViewModel(Guid.NewGuid())));
+        }
+    }
+}
